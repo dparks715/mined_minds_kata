8e14140e21eac22b765ec78a8044be568e3d0802
Daniel Parks <dparks715@yahoo.com>
2017-01-17 05:21:43 -0500

    added spacing for clarity

--- test_mined_minds_function.rb
+++ test_mined_minds_function.rb
@@ -13,6 +13,7 @@ class TestMMfunction < Minitest::Test
 	def test_three_returns_mined()
 		assert_equal('mined', get_mined_minds_result(3))
 	end
+
 # checks that 15, 30, 45, 60, 75, 90 return mined minds
 	def test_divisible_15_returns_minedminds()
 		num = 15
@@ -27,18 +28,22 @@ class TestMMfunction < Minitest::Test
 	def test_five_returns_minds()		
 			assert_equal('minds', get_mined_minds_result(5))
 	end
+
 # checks that 10 retuns TEN!
 	def test_10_returns_TEN!()
 		assert_equal('TEN!', get_mined_minds_result(10))
 	end
+
 # checks 91 returns 91
 	def test_ninetyone_returns_ninetyone()
 		assert_equal(91, get_mined_minds_result(91))
 	end
+
 # checks 37 returns taco tuesday
 	def test_thirtyseven_returns_tacotues()
 		assert_equal('taco tuesday', get_mined_minds_result(37))
 	end
+	
 # checks 22 returns 22
 	def test_22_returns_22()
 		assert_equal(22, get_mined_minds_result(22))
